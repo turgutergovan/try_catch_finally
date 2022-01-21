2f4426c75475a4c6af22d32c85c705faf221941c
Sabahci <Sabahci@DESKTOP-6C2KRTE>
2022-01-21 15:03:16 +0300

    Add project files.

---
+++ try_catch_finally/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace try_catch_finally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                int sayi1 = 1;
+                int sayi2 = 0;
+                int sonuc;
+                sonuc = sayi1 / sayi2;
+                Console.WriteLine(sonuc);
+            }
+            catch(FormatException fe)
+            {
+                throw fe;
+            }
+            catch(DivideByZeroException dbze)
+            {
+                throw dbze;
+            }
+            catch (Exception hata)
+            {
+                Console.WriteLine(hata);
+              
+            }
+            finally
+            {
+                Console.WriteLine("Finally kod bloger");
+            }
+        }
+    }
+}
